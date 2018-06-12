302717ff706542608d10341080e75ce672187daf
TECH-A50 <tech-a50@TECH-A50s-MacBook-Air.local>
2018-06-12 11:00:40 +0100

    First commit

---
+++ calculator.rb
@@ -0,0 +1,29 @@
+#Calculator
+
+#Prompt for mode: Basic or Advanced
+puts "Select mode: Basic(b) or Advanced(a)"
+mode = gets.chomp
+
+
+#Basic mode
+
+  #Prompt for function
+  #Prompt for num1
+  #Prompt for num2
+
+  #Prompt for function
+    #Addition
+    #return result
+
+    #Subtract
+    #return result
+
+    #Multiply
+    #return result
+
+    #Divide
+    #return result
+
+#Advanced mode
+  #Prompt for num1
+  #Prompt for num2
